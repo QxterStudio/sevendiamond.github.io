3e68bb374be6ddc5cb4e2fd1fcd5fa3cde7c885a
QxterStudio <92514260+QxterStudio@users.noreply.github.com>
2022-11-24 19:29:29 +0900

    Add files via upload

---
+++ lesson/test.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.link;
+using System.text;
+using System.Threading.Task;
+
+namespace test
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine('text')
+        }
+    }
+}
